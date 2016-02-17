2043402eeaa764f3197eff6d79542d73224b38c2
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-02-17 15:43:36 +0900

    ported admin stuff from dcares

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :modes
   root 'bids#index'
+  get '/admin_panel' => 'users#admin'
 
+  resources :modes
   resources :bids
   devise_for :users
+  scope "/admin" do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217050022) do
+ActiveRecord::Schema.define(version: 20160217061351) do
 
   create_table "bids", force: :cascade do |t|
     t.string   "title",             limit: 255
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160217050022) do
     t.boolean  "is_approved",                        default: false
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
