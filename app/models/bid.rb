961f7191a6c93c09f9b430f0d570ba93b524dfc8
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-03-31 11:47:50 +0900

    removed required constraints from bidding fields

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,19 +1,19 @@
 class Bid < ActiveRecord::Base
 	validates :title, presence: true
-	validates :contractor, presence: true
+	# validates :contractor, presence: true
 	validates :mode, numericality: { :greater_than => 0 }, presence: true
 	validates :number, numericality: { :greater_than_or_equal_to => 0 }, presence: true
 	validates :budget, numericality: { :greater_than_or_equal_to => 0 }, presence: true
-	validates :amount, numericality: { :greater_than_or_equal_to => 0 }, presence: true
+	# validates :amount, numericality: { :greater_than_or_equal_to => 0 }, presence: true
 
-	validates_presence_of :preprocurement, if: :is_public_bidding?
-    validates_presence_of :prebidding, if: :is_public_bidding?
-    validates_presence_of :bidding, if: :is_public_bidding?
-    validates_presence_of :postqualification, if: :is_public_bidding?
-    validates_presence_of :noa, if: :is_public_bidding?
-    validates_presence_of :purchase, if: :is_public_bidding?
-    validates_presence_of :ntp, if: :is_public_bidding?
-    validates_presence_of :members, if: :is_public_bidding?
+	# validates_presence_of :preprocurement, if: :is_public_bidding?
+ #    validates_presence_of :prebidding, if: :is_public_bidding?
+ #    validates_presence_of :bidding, if: :is_public_bidding?
+ #    validates_presence_of :postqualification, if: :is_public_bidding?
+ #    validates_presence_of :noa, if: :is_public_bidding?
+ #    validates_presence_of :purchase, if: :is_public_bidding?
+ #    validates_presence_of :ntp, if: :is_public_bidding?
+ #    validates_presence_of :members, if: :is_public_bidding?
 
 	searchkick
 
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -7,40 +7,40 @@ RSpec.describe Bid, type: :model do
 
   describe "ActiveModel Validations" do
   	it { should validate_presence_of(:title) }
-  	it { should validate_presence_of(:contractor) }
+  	# it { should validate_presence_of(:contractor) }
   	it { should validate_presence_of(:number) }
   	it { should validate_numericality_of(:number).is_greater_than_or_equal_to(0) }
   	it { should validate_presence_of(:mode) }
   	it { should validate_numericality_of(:mode).is_greater_than(0) }
     it { should validate_presence_of(:budget) }
     it { should validate_numericality_of(:budget).is_greater_than_or_equal_to(0) }
-    it { should validate_presence_of(:amount) }
-    it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
+    # it { should validate_presence_of(:amount) }
+    # it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
 
-    context "Procured via Public Bidding" do
-      before(:each) { allow(subject).to receive(:is_public_bidding?).and_return(true) }
+    # context "Procured via Public Bidding" do
+    #   before(:each) { allow(subject).to receive(:is_public_bidding?).and_return(true) }
 
-      it { should validate_presence_of(:preprocurement) }
-      it { should validate_presence_of(:prebidding) }
-      it { should validate_presence_of(:bidding) }
-      it { should validate_presence_of(:postqualification) }
-      it { should validate_presence_of(:noa) }
-      it { should validate_presence_of(:purchase) }
-      it { should validate_presence_of(:ntp) }
-      it { should validate_presence_of(:members) }
-    end
+    #   it { should validate_presence_of(:preprocurement) }
+    #   it { should validate_presence_of(:prebidding) }
+    #   it { should validate_presence_of(:bidding) }
+    #   it { should validate_presence_of(:postqualification) }
+    #   it { should validate_presence_of(:noa) }
+    #   it { should validate_presence_of(:purchase) }
+    #   it { should validate_presence_of(:ntp) }
+    #   it { should validate_presence_of(:members) }
+    # end
 
-    context "Other modes of procurement" do
-      before(:each) { allow(subject).to receive(:is_public_bidding?).and_return(false) }
+    # context "Other modes of procurement" do
+    #   before(:each) { allow(subject).to receive(:is_public_bidding?).and_return(false) }
 
-      it { should_not validate_presence_of(:preprocurement) }
-      it { should_not validate_presence_of(:prebidding) }
-      it { should_not validate_presence_of(:bidding) }
-      it { should_not validate_presence_of(:postqualification) }
-      it { should_not validate_presence_of(:noa) }
-      it { should_not validate_presence_of(:purchase) }
-      it { should_not validate_presence_of(:ntp) }
-      it { should_not validate_presence_of(:members) }
-    end
+    #   it { should_not validate_presence_of(:preprocurement) }
+    #   it { should_not validate_presence_of(:prebidding) }
+    #   it { should_not validate_presence_of(:bidding) }
+    #   it { should_not validate_presence_of(:postqualification) }
+    #   it { should_not validate_presence_of(:noa) }
+    #   it { should_not validate_presence_of(:purchase) }
+    #   it { should_not validate_presence_of(:ntp) }
+    #   it { should_not validate_presence_of(:members) }
+    # end
   end
 end
